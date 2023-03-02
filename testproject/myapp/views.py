f4b0e8d18f5974648ca053b8fda56f634d4f9062
Dmitrii Savickii <savickii.d@gmail.com>
2023-03-02 22:00:39 +0300

    commit 4

--- testproject/myapp/views.py
+++ testproject/myapp/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    data = {
-        'title': 'Главная страница!!',
-        'mylist': ['Hello', 'Python', 'is', 'awesome!']
-    }
-    return render(request, 'myapp/index.html', data)
+    return render(request, 'myapp/index.html')
 
 def about(request):
     return render(request, "myapp/about.html")
